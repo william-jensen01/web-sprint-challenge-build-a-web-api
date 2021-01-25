864215e05fe72d262c43069676afc9d14a4bab31
William Jensen <williambjensen01@gmail.com>
2021-01-24 18:40:32 -0700

    MVP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,61 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model');
+
+// middleware
+const { validateActionId, validateActionBody } = require('../middleware');
+
+// router for /api/actions
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Actions.get()
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(next)
+})
+router.get('/:id', validateActionId, (req, res, next) => {
+    const { id } = req.params;
+    Actions.get(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).json({ message: "Sorry, the specified ID does not exist" })
+            } else {
+                res.status(200).json(data)
+            }
+        })
+        .catch(next)
+})
+router.post('/', validateActionBody, (req, res, next) => {
+    Actions.insert(req.body)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(next)
+})
+router.put('/:id', validateActionId, validateActionBody, (req, res, next) => {
+    const { id } = req.params
+    Actions.update(id, req.body)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(next)
+})
+router.delete('/:id', validateActionId, (req, res, next) => {
+    Actions.remove(req.params.id)
+        .then(deleted => {
+            if (deleted ===1) {
+                res.status(200).json({ message: `The action with the id of ${req.params.id} was deleted.` })
+            } else {
+                res.status(404).json({ message: `The action with the id of ${req.params.id} was not found.` })
+            }
+        })
+        .catch(next)
+})
+
+router.use((error, req, res, next) => {
+    res.status(500).json({ info: "There was an error in the router", message: error.message, stack: error.stack })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
+
 const server = express();
 
 // Complete your server here!
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
